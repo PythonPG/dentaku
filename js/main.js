9aa8668ea42e76853bc94b35fc2c711558e0928f
PythonPG <cloud9slack@gmail.com>
2018-07-10 18:17:29 +0900

    電卓動くバージョン

--- js/main.js
+++ js/main.js
@@ -1,17 +1,74 @@
-(function() {
-  var Dentaku = document.dentaku;
-  var FlagNum = false;
-  function NumPress (Num) {
+var Dentaku = document.dentaku;
+var FlagNum = false;
+var Accumulation = 0;
+var Enzan = "";
+
+function NumPress (Num) {
     if (FlagNum) { 
-      Dentaku.readvalue.value  = Num;
+      Dentaku.readvalue.value = Num;
       FlagNum = false;
     }
     else {
-      if (FlagNum.readvalue.value == "0") {
+      if (Dentaku.readvalue.value == "0") {
         Dentaku.readvalue.value = Num;
       } else {
         Dentaku.readvalue.value += Num;
       }
     }
   }
-})();
+
+function Operation (Op) {
+  var Readout = Dentaku.readvalue.value;
+    if (FlagNum && Enzan != "=");
+    else {
+      FlagNum = true;
+        if ( Enzan == '+' ) 
+          Accumulation += parseFloat(Readout);
+        else if ( '-' == Enzan ) 
+          Accumulation -= parseFloat(Readout);
+        else if ( '/' == Enzan ) 
+          Accumulation /= parseFloat(Readout);
+        else if ( '*' == Enzan )
+          Accumulation *= parseFloat(Readout);
+        else if ( '%' == Enzan )
+          Accumulation %= parseFloat(Readout);
+        else
+          Accumulation = parseFloat(Readout);
+          Dentaku.readvalue.value = Accumulation;
+          Enzan = Op;
+    }
+}
+
+function Shosuten () {
+  var nowReadOut = Dentaku.readvalue.value;
+  if (FlagNum) {
+    nowReadOut = "0.";
+    FlagNum = false;
+  } else {
+    if (nowReadOut.indexOf(".") == -1)
+      nowReadOut += ".";
+  }
+  Dentaku.readvalue.value = nowReadOut;
+}
+
+function ClearEntry () {
+  Dentaku.readvalue.value = "0";
+  FlagNum = true;
+}
+
+function Clear () {
+  Accumulation = 0;
+  Enzan = "";
+  ClearEntry();
+}
+
+function PlusMinus () {
+  Dentaku.readvalue.value = parseFloat(Dentaku.readvalue.value) * -1;
+}
+
+function Percent () {
+  if ( Enzan == '+' || Enzan == '-' ) 
+  Dentaku.readvalue.value = (parseFloat(Dentaku.readvalue.value) / 100) * parseFloat(Accumulation);
+  else if ( Enzan == '*' || Enzan == '/' )
+  Dentaku.readvalue.value = parseFloat(Dentaku.readvalue.value) / 100;
+}
